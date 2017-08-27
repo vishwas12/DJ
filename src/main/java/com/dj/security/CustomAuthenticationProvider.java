ec526529948aeaa75bc68996351951b61d888351
Vishwas Ganguly <vishwasg@dewsolutions.in>
2017-08-27 16:20:31 +0530

    custom security added

--- src/main/java/com/dj/security/CustomAuthenticationProvider.java
+++ src/main/java/com/dj/security/CustomAuthenticationProvider.java
@@ -1,10 +1,15 @@
 package com.dj.security;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import com.dj.dto.User;
@@ -26,9 +31,19 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         StandardPasswordEncoder sp = new StandardPasswordEncoder();
         
         boolean passwordMatch=false;
-        User user = null;
+        User user = new User();
+        user.setFirstName("Vishwas");
+        user.setLastName("Ganguly");
+        user.setEmail("simplyganguly4u@gmail.com");
+        user.setPassword(password);
         
-		return null;
+        List<GrantedAuthority> grantedAuths = new ArrayList<>();
+        grantedAuths.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        
+        
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, password, grantedAuths);
+        
+		return auth;
 	}
 
 	@Override
